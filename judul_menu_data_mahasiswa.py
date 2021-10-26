0c2670f1affc1f11e5d2c929742a18cfdacc0b8c
Natalieefd <93095344+Natalieefd@users.noreply.github.com>
2021-10-26 20:08:21 +0800

    Add files via upload

---
+++ judul_menu_data_mahasiswa.py
@@ -0,0 +1,19 @@
+
+def judul():
+    print("\n--------------------------------------")
+    print("          Data Absen Mahasiswa")
+    print("--------------------------------------")
+
+def menu1():
+    print("                MENU                  ")
+    print("--------------------------------------")
+    print(" [1] Tambah Data Mahasiswa")
+    print(" [2] Lihat Data Mahasiswa")
+    print(" [3] Hapus Data Mahasiswa")
+    print(" [4] Exit")
+    print("--------------------------------------")
+
+def exit():
+    print("--------------------------------")
+    print("          TERIMA KASIH!")
+    print("--------------------------------")
---
+++ judul_menu_konversi_suhu.py
@@ -0,0 +1,20 @@
+def judul():
+    print("\n------------------------------------------------------")
+    print("Mengkonversi Fahrenheit, Kelvin, dan Reamur ke Celcius")
+    print("------------------------------------------------------")
+
+
+def menu():
+    print("                        MENU                          ")
+    print("------------------------------------------------------")
+    print(" [1] Fahrenheit ke Celcius")
+    print(" [2] Kelvin ke Celcius")
+    print(" [3] Reamur ke Celcius")
+    print(" [4] Exit")
+    print("------------------------------------------------------\n")
+
+
+def exit():
+    print('\n---------------------------------------\n')
+    print('             TERIMA KASIH!')
+    print('\n---------------------------------------')
